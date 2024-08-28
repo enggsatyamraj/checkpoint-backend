6ad67aae9e2388e08dd33038887f03379afba2c4
enggsatyamraj <sattu62025@gmail.com>
2024-08-28 16:34:54 +0530

    setup the backend, and created the server

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const database = require("./config/database");
+
+const app = express();
+app.use(express.json());
+
+dotenv.config();
+
+app.use(cors());
+
+database.connect();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "your server is up and running......",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`your server is running on port ${PORT}`);
+});
