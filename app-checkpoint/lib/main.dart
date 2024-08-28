68380c30c5d17090cd9a51e2bf08ce1a44f943d6
Akshay Negi <160517540+akshayAtDWI@users.noreply.github.com>
2024-08-28 14:26:29 +0530

    created flutter app

---
+++ app-checkpoint/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:checkpoint/app/app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+    ]);
+  runApp(const MyApp());
+}
