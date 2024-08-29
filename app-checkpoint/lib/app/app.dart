297d45c9ae12af9d984b4be6e3bb3d824b02077e
Akshay Negi <160517540+akshayAtDWI@users.noreply.github.com>
2024-08-29 10:22:48 +0530

    initialised app

--- app-checkpoint/lib/app/app.dart
+++ app-checkpoint/lib/app/app.dart
@@ -1,5 +1,7 @@
+import 'package:checkpoint/themes/theme.dart';
 import 'package:checkpoint/views/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,27 +10,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Checkpoint',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+              useMaterial3: true,
+              colorScheme: MyThemes.lightColorScheme,
+              fontFamily: GoogleFonts.nunitoSans().fontFamily),
+          darkTheme: ThemeData(
+              useMaterial3: true,
+              colorScheme: MyThemes.darkColorScheme,
+              fontFamily: GoogleFonts.nunitoSans().fontFamily),
+          themeMode: ThemeMode.system,
+          
+      home: const Homepage(),
     );
   }
 }
