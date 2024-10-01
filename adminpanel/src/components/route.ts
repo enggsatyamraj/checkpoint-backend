13277b9de803ce84304967feb704ec0d03a6f6b8
enggsatyamraj <sattu62025@gmail.com>
2024-10-01 16:57:45 +0530

    initial commit

--- adminpanel/src/components/route.ts
+++
@@ -1,2 +0,0 @@
-export const basePath = "https://checkpoint-awvj.onrender.com/api/v1";
-// export const basePath = "http://localhost:3000/api/v1";
--- app-checkpoint/android/app/src/main/kotlin/com/example/checkpoint/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.techfusion.checkpoint
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
